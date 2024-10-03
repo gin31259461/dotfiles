048546bc5ea6afc7305b3bd27d9552e9d61d1bb3
Abner <qw0207060413@gmail.com>
2024-10-03 15:08:07 +0800

    Sync dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,12 +2,16 @@ cd $HOME
 
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-dot add README.md
-dot add dotfiles.sh
-dot add .zshrc .p10k.zsh
+dot add README.md dotfiles.sh .gitconfig
+
+dot add .zshrc .zprofile .p10k.zsh
+
 dot add .config/code-flags.conf .config/electron-flags.conf \
   .config/nvim .config/hypr .config/kitty .config/nvim-vscode \
-  .config/wallust .config/waybar .config/wlogout .config/bat
+  .config/wallust .config/waybar .config/wlogout .config/bat \
+  .config/btop .config/cava .config/rofi .config/Kvantum \
+  .config/ags .config/fastfetch .config/qt5ct .config/qt6ct \
+  .config/swappy .config/swaync
 
 dot commit -m "Sync dotfiles"
 dot push origin main
