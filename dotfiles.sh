bc4a5e9db5caca7364f32aa3539894bd45b4047c
abner <qw0207060413@gmail.com>
2025-02-05 13:10:04 +0800

    Sync dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,6 +8,8 @@ dot add .p10k.zsh .zshrc
 
 dot add .config/hypr/UserConfigs
 dot add .config/hypr/UserScripts
+dot add .config/hypr/README.md
+
 dot add .config/foot/
 
 dot commit -m "Sync dotfiles"
