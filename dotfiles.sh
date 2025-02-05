38be676a7e2199ad905f0774e569d1f7119298e2
abner <qw0207060413@gmail.com>
2025-02-05 15:48:44 +0800

    Sync dotfiles

--- dotfiles.sh
+++ dotfiles.sh
@@ -2,15 +2,23 @@ cd $HOME
 
 alias dot='git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
+# git
 dot add README.md dotfiles.sh .gitconfig
 
+# zsh
 dot add .p10k.zsh .zshrc
 
+# hyprland
 dot add .config/hypr/UserConfigs
 dot add .config/hypr/UserScripts
 dot add .config/hypr/README.md
 
+# foot terminal
 dot add .config/foot/
 
+# swaylock-effects
+dot add .config/swaylock/
+
+# push
 dot commit -m "Sync dotfiles"
 dot push origin main
